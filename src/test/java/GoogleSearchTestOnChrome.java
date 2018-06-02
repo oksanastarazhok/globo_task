fb4bcf63a266577c332859a7e17365ca4a941947
oksanastarazhok <Oksana_Starazhok@epam.com>
2018-06-02 16:51:57 +0300

    added test client

--- src/test/java/GoogleSearchTestOnChrome.java
+++ src/test/java/GoogleSearchTestOnChrome.java
@@ -1,45 +1,67 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.io.File;
+import java.io.IOException;
+
 public class GoogleSearchTestOnChrome {
     private WebDriver driver;
 
     @BeforeTest
     public void setUp() {
-        System.setProperty( "webdriver.chrome.driver",
-                "./src/test/resources/drivers/chromedriver.exe" );
+        System.setProperty("webdriver.chrome.driver",
+                "./src/test/resources/drivers/chromedriver.exe");
 
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.get( "http://www.google.com" );
+        driver.get("https://staging-web1.corp.globoforce.com/microsites/t/home?client=testclientclone2&setCAG=true");
     }
 
     @Test
-    public void testGoogleSearch() {
-        WebElement element = driver.findElement( By.name( "q" ) );
+    public void testGoogleSearch() throws Exception {
+
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("username")));
+
+        WebElement username = driver.findElement(By.name("username"));
+        username.sendKeys("79961T");
+        WebElement password = driver.findElement(By.name("password"));
+        password.sendKeys("password1");
+        WebElement loginBtn = driver.findElement(By.id("signIn-button"));
+        loginBtn.click();
+
+        Boolean title = driver.getTitle().contains("Welcome");
 
 
-        element.sendKeys( "Globoforce" );
+        if (!title) {
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"login-ui-app\"]/login-ui-app/legal-component/div/button")));
+            WebElement legalBtn = driver.findElement(By.xpath("//*[@id=\"login-ui-app\"]/login-ui-app/legal-component/div/button"));
+            legalBtn.click();
+        } else {
+            System.out.println("Aggreement has been accepted earlier.");
+        }
 
-        element.submit();
 
-        new WebDriverWait( driver, 10 ) {
-            public Boolean apply(WebDriver d) {
-                return d.getTitle().toLowerCase()
-                        .startsWith( "globoforce" );
-            }
-        };
-        String title = driver.getTitle().substring( 0, 10 );
-        Assert.assertEquals( "Globoforce",
-                title );
+        File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        try {
+
+            FileUtils.copyFile(src, new File(".\\src\\test\\screenshots\\screenshot" + System.currentTimeMillis() + ".png"));
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+
+
+        }
+        String homePage = driver.getCurrentUrl().substring(0,65);
+        Assert.assertEquals(homePage,"https://staging-web1.corp.globoforce.com/microsites/t/home?client");
     }
+
 
     @AfterTest
     public void tearDown() throws Exception {
